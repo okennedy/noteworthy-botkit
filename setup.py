43884f59fd2a0327fbbe22bc09743d3fa89bc1ff
Patrick De La Garza <Patrickdlg@gmail.com>
2020-08-06 14:00:03 -0700

    include README in package

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='matrixbz',
       url="https://decentralabs.io",
       author_email="hi@decentralabs.io",
-      version='0.0.1',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      version='0.0.2',
       license='AGPLv3',
       packages=find_packages(),
       zip_safe=False,
